b645dce4aa6d1cd5b67e2d531e2ca8db74ed9cf9
skoshx <rasmus.gustafsson2611@gmail.com>
2025-09-19 22:01:11 +0300

    fix: handle errors in brew install and fix path

--- brew/install.sh
+++ brew/install.sh
@@ -1,22 +1,27 @@
 #!/bin/zsh
 
 function install_casks {
-  local casks_file="./casks"
+  local casks_file="./brew/casks"
   while IFS= read -r line || [[ -n "$line" ]]; do
     [[ -z "$line" || "$line" == \#* ]] && continue  # Skip empty lines and comments
-    brew install --cask "$line"
+    echo "installing: $line"
+    if ! brew install --cask "$line"; then
+      echo "failed to install: $line"
+    fi
   done < "$casks_file"
 }
 
 function install_leaves {
-  local leaves_file="./leaves"
+  local leaves_file="./brew/leaves"
   while IFS= read -r line || [[ -n "$line" ]]; do
     [[ -z "$line" || "$line" == \#* ]] && continue  # Skip empty lines and comments
-    brew install "$line"
+    echo "installing: $line"
+    if ! brew install "$line"; then
+      echo "failed to install: $line"
+    fi
   done < "$leaves_file"
 }
 
 echo "installing programs with brew"
 install_casks
 install_leaves
-
